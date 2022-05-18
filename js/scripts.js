7c47cd6f143975ca5ccbcf27b8889cf00595e02c
mustaque <mustaqueny@icloud.com>
2022-05-18 15:20:42 -0400

    edit javascript file and add to Branch take-home

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 
+// Single line comment
+/* Multi-line comments
+comments can be used to explain JavaScript code, and to make it more readable.
+comments can also be used to prevent execution, when testing alternative code.
+*/
+
+console.log("Hi I'm Mustaque");
+
+console.log("My favorite food is Bihari Kabab");
 
